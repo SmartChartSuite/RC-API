21df3822cf5d0c6ca3a2ed51d77a52a08e975242
Andy Stevens <andrew.stevens@gtri.gatech.edu>
2021-12-02 09:57:32 -0500

    Fixing docs URL

--- app/main.py
+++ app/main.py
@@ -58,8 +58,8 @@ async def custom_swagger_ui_html():
         openapi_url=app.openapi_url,
         title=app.title + " - Swagger UI",
         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="rc-api/static/swagger-ui-bundle.js",
-        swagger_css_url="rc-api/static/swagger-ui.css",
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
     )
 
 
