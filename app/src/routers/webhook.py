e275ee6892a4ab7e6a9adf84aa7d72c5e48beaed
Ellie Shivers <elliemshivers@gmail.com>
2022-03-14 16:13:05 -0400

    Fixing webhook request

--- app/src/routers/webhook.py
+++ app/src/routers/webhook.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Request
 
 from ..util.git import clone_repo_to_temp_folder
 import logging
@@ -7,7 +7,8 @@ logger = logging.getLogger('rcapi.routers.webhook')
 apirouter = APIRouter()
 
 @apirouter.post("/webhook")
-def webhook(message: dict = Body(...)):
+def webhook(request: Request):
+    message = request.json
     # TODO: Determine whether to use SSH or HTTPS from config
     clone_url = message['repository']['clone_url']
     ssh_url = message['repository']['ssh_url']
