6b80571be6400bda1f9504dd0646cf42b172c908
Andy Stevens <andrew.stevens@gtri.gatech.edu>
2022-04-28 17:57:15 -0400

    Update functions.py
    Remove sleep to not cause hanging

--- src/models/functions.py
+++ src/models/functions.py
@@ -104,7 +104,6 @@ def run_nlpql(library_ids: list, patient_id: str, external_fhir_server_url_strin
         # Start running jobs
         future = session.post(nlpaas_url + job_url, json=nlpql_post_body)
         futures.append(future)
-        sleep(3)
     return futures
 
 
